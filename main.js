7842c37e9486028e51a58972aa8d93eafe1a99e4
Ruslan <erkebaevruslan@gmail.com>
2025-01-16 04:21:13 +0600

    Creating H/W-16

---
+++ main.js
@@ -0,0 +1,61 @@
+// 1 Задача
+
+// let num = prompt("Сан жазыныз");
+// if (num == 10) {
+//   console.log("Туура");
+// } else {
+//   console.log("Туура эмес");
+// }
+
+// 2 Задача
+
+// let pod = prompt("Введите номер квартиры");
+// if (pod > 0 && pod < 21) {
+//   console.log("Первый подъезд");
+// } else if (pod > 20 && pod < 49) {
+//   console.log("Второй подъезд");
+// } else if (pod > 48 && pod < 91) {
+//   console.log("Третий подъезд");
+// } else {
+//   console.log("Пашол вон");
+// }
+
+// 3 Задача
+
+// let num = 7;
+// if (num > 0 && num < 5) {
+//   console.log("Туура");
+// } else {
+//   console.log("Туура эмес");
+// }
+
+// 4 Задача
+
+// let clock = prompt("Введите время");
+// if (clock > 5 && clock < 13) {
+//   alert("С добрым утром!");
+// } else if (clock > 12 && clock < 18) {
+//   alert("Добрый день!");
+// } else {alert
+//   ("Добрый вечер!");
+// }
+
+// 5 Задача
+
+// let clock = prompt("Убакытты жазыныз");
+// if (clock > 5 && clock < 13) {
+//   alert("Кутмандуу таныныз менен Samira!");
+// } else if (clock > 12 && clock < 18) {
+//   alert("Кутмандуу кунунуз менен Samira!");
+// } else {
+//   alert("Кутмандуу кечиниз менен Samira!");
+// }
+
+// 6 Задача
+
+// let sale = prompt("Введите возраст");
+// if (sale <= 6 && sale >= 65) {
+//   console.log("Со скидкой");
+// } else {
+//   console.log("Без скидки");
+// }
