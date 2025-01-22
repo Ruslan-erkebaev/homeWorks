9ec5a710038e051e61731006a674e3cffac5fde5
Ruslan <erkebaevruslan@gmail.com>
2025-01-23 03:16:25 +0600

    start H/W-18

---
+++ main.js
@@ -0,0 +1,14 @@
+// Задача 1
+// const getNum = (nubers) => {
+//   console.log(nubers);
+// };
+// getNum('1, 2, 3, 4, 5, 6, 7, 8, 9, 0');
+
+// Задача 2 неправильно
+let checkNumbers = 24;
+const numbers = () => {
+  if (checkNumbers % 2 === 0) {
+    console.log(checkNumbers);
+  }
+};
+numbers();
